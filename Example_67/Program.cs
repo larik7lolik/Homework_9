7285a9317a0e966c4a027fca25d06ae0143a0532
LarisaZaripova <larik7lolik@gmail.com>
2022-03-01 13:27:21 +0300

    added Example 67

---
+++ Example_67/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 67: Показать натуральные числа от N до 1, N задано.
+//N = 5. -> "5, 4, 3, 2, 1"
+//Console.Clear();
+Console.Clear();
+int List(int n)
+{
+    if(n == 0) return 0;
+    Console.Write(n + "  " );
+    return List(n-1);
+}
+Console.WriteLine("Введите границу интервала: ");
+int n = int.Parse(Console.ReadLine());
+List(n);
+Console.WriteLine();
